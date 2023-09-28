9e05f400143f3cf93bb00b30c242d0e20809e68d
James Edmond <jamesedmond@pursuit.org>
2023-09-28 09:30:29 -0400

    finished lab

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,18 @@
-export default function Progress() {
-  return null;
+import React from "react";
+
+export default function Progress({donations, target}) {
+  let sum = 0
+  for(let donation of donations) {
+    sum += donation.amount
+  }
+  console.log(sum)
+
+  return (
+    <section className="progress">
+      <h2>
+      Raised <span className="secondary">${sum}</span> of
+        <span className="secondary"> ${target}</span>
+      </h2>
+    </section>
+  );
 }
